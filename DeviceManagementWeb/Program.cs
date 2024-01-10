09bd9ef099ea2965ed8cae55d4cf8513760de83b
Mihaiela Nagy <mihaelacoubis@gmail.com>
2024-01-10 09:47:19 +0200

    Configured Swagger to accept Header Authorization

--- DeviceManagementWeb/Program.cs
+++ DeviceManagementWeb/Program.cs
@@ -5,6 +5,7 @@ using DeviceManagementWeb.Services;
 using DeviceManagementWeb.Services.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Net.Http;
 using System.Text;
 using OperatingSystem = DeviceManagementDB.Models.OperatingSystem;
@@ -62,7 +63,38 @@ builder.Services.AddAuthentication(opt =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Device Management API", Version = "v1" });
+
+    // Add the following code for JWT Authorization
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
 builder.Services.AddCors(options => options.AddPolicy(name: "DeviceOrigins",
     policy =>
     {
@@ -76,7 +108,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWTAuthDemo v1"));
 }
 
 app.UseCors("DeviceOrigins");
